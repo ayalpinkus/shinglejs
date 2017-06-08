153a1ba22b9418a5ba9055a6d7427e46585c33cc
Ayal Pinkus <apinkus@xs4all.nl>
2017-06-08 18:45:53 +0200

    Small fixes.

--- extra_src/build-data-jacek.sh
+++ extra_src/build-data-jacek.sh
@@ -6,14 +6,10 @@ DATASET=./
 echo "dataset = $DATASET"
 
 /home/ayalpinkus/Work/LowPriority/shinglejs/bin/json2array_jacek.a $DATASET testnodes.bin testedges.bin
-
-#rm -rf ./datasets/elsevier-data
-
 mkdir -p ./datasets/elsevier-data/
 
 
 echo "Create bitmaps"
-
 /home/ayalpinkus/Work/LowPriority/shinglejs/bin/makebitmap_elsevier.a 2400 0.33 testnodes.bin image_2400.pnm
 pnmscale 0.5 image_2400.pnm > image_1200.pnm
 pnmscale 0.4 image_2400.pnm > image_960.pnm
@@ -26,7 +22,6 @@ cjpeg image_960.pnm > image_960.jpg
 cjpeg image_800.pnm > image_800.jpg
 cjpeg image_600.pnm > image_600.jpg
 cp image_1200.jpg image_2400low.jpg
-
 cp *.jpg datasets/elsevier-data/
 
 
